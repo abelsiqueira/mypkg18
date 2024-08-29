ea81697023855874f37e222955fa9d155b9517c5
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2024-08-29 21:35:54 +0200

    First commit

---
+++ src/mypkg18/__init__.py
@@ -0,0 +1,8 @@
+"""Documentation about mypkg18."""
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Abel"
+__email__ = "abel@abel.com"
+__version__ = "0.1.0"
